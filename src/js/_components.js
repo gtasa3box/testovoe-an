594a390d8932ac8fa8f234ca31911262da558a7a
gtasa3box <gtasa3box@gmail.com>
2022-03-27 14:36:22 +0300

    com

--- src/js/_components.js
+++ src/js/_components.js
@@ -3,3 +3,11 @@ $('.lang__selected').click(function(){
     $(this).toggleClass('active');
     $($(this).data("target")).stop().slideToggle();
 });
+
+//promocodes
+$('#promo-btn').click(function(){
+    event.preventDefault();
+    $(this).toggleClass('active');
+    $($(this).data("target")).addClass('active');
+    $('#void-block').addClass('hidden');
+});
