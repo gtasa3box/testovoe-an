5141010d7bc0437036f3f19a5cce6dfa22e55a7e
gtasa3box <gtasa3box@gmail.com>
2022-03-27 17:29:02 +0300

    com

--- src/js/_components.js
+++ src/js/_components.js
@@ -11,3 +11,21 @@ $('#promo-btn').click(function(){
     $($(this).data("target")).addClass('active');
     $('#void-block').addClass('hidden');
 });
+
+//collapse-sidebar
+$('#btn-collapse-sidebar').click(function(){
+    $(this).toggleClass('collapse');
+    $('.lc-sidebar').toggleClass('collapse');
+    $('.page__body_lc').toggleClass('collapse');
+    $('.btn_money').toggleClass('collapse');
+    $('.user-card').toggleClass('collapse');
+    $('.nav-sidebar').toggleClass('collapse');
+});
+
+//burger
+$('.burger').click(function(){
+    $(this).toggleClass('active');
+    $('.lc-sidebar__user-logo-mob').toggleClass('hidden');
+    $('.lc-sidebar').toggleClass('active-mob');
+    $('.lc-sidebar__content').toggleClass('active-mob');
+});
